6d2f4438b00fd0ae98f52db11f4c27e0bd07c805
Alepha Null <HashTag>
2023-12-07 00:36:09 +0530

    Made it prettier

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -7,25 +7,77 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 const basePromptPrefix =
   `
-  Below is the name of an author. Suggest some books that in the same genre of the other but make sure to not suggest any books by the authot themselves
+Find out what genre this author writes in and also similar genres.
 
-  Author: 
+Author:
 `
+
 const generateAction = async (req, res) => {
-  // Run first prompt
   console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
 
   const baseCompletion = await openai.createCompletion({
     model: 'text-davinci-003',
-    prompt: `${basePromptPrefix}${req.body.userInput}\n`,
-    temperature: 0.7,
+    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    temperature: 0.8,
     max_tokens: 250,
   });
 
   const basePromptOutput = baseCompletion.data.choices.pop();
 
-  res.status(200).json({ output: basePromptOutput });
+  // I build Prompt #2.
+  const secondPrompt =
+    `
+
+  Suggest some good books in this genre without reccomending books from this authot.
+
+  Author: ${req.body.userInput}
+
+  Genre: ${basePromptOutput.text}
+
+  Books:
+  `
+
+  // I call the OpenAI API a second time with Prompt #2
+  const secondPromptCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${secondPrompt}`,
+    // I set a higher temperature for this one. Up to you!
+    temperature: 0.85,
+    // I also increase max_tokens.
+    max_tokens: 1250,
+  });
+
+  // Get the output
+  const secondPromptOutput = secondPromptCompletion.data.choices.pop();
+
+  // Send over the Prompt #2's output to our UI instead of Prompt #1's.
+  res.status(200).json({ output: secondPromptOutput });
 };
 
 export default generateAction;
 
+
+// const basePromptPrefix =
+//   `
+//   Below is the name of an author. Suggest some books that in the same genre of the other but make sure to not suggest any books by the authot themselves
+//
+//   Author: 
+// `
+// const generateAction = async (req, res) => {
+//   // Run first prompt
+//   console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+//
+//   const baseCompletion = await openai.createCompletion({
+//     model: 'text-davinci-003',
+//     prompt: `${basePromptPrefix}${req.body.userInput}\n`,
+//     temperature: 0.7,
+//     max_tokens: 250,
+//   });
+//
+//   const basePromptOutput = baseCompletion.data.choices.pop();
+//
+//   res.status(200).json({ output: basePromptOutput });
+// };
+//
+// export default generateAction;
+//
